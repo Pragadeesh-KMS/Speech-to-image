df93e506f08904a5ee511e0bd6847be4f9645041
Pragadeesh KMS <86162622+Pragadeesh-KMS@users.noreply.github.com>
2024-01-12 12:54:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from transformers import pipeline
-from diffusers import DiffusionPipeline, AutoPipelineForText2Image
+
+whisper = pipeline("automatic-speech-recognition", model = "openai/whisper-large-v2", chunk_length_s=30, device = "cuda:0")
+
+from diffusers import StableDiffusionXLPipeline
+
+from diffusers import AutoPipelineForText2Image
 import torch
-from IPython.display import display, Javascript, Image
+from IPython.display import HTML, Javascript, Image, display
 from google.colab.output import eval_js
 import base64
+pipe = StableDiffusionXLPipeline.from_pretrained("segmind/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
 from hugchat import hugchat
 from hugchat.login import Login
 
-whisper = pipeline("automatic-speech-recognition", model="openai/whisper-large-v2", chunk_length_s=30, device="cuda:0")
-pipeline = DiffusionPipeline.from_pretrained("stabilityai/sdxl-turbo")
-pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
-pipe.to("cuda")
+email = "kmspragadeesh6000@gmail.com"
 
-email = "Hugging face mail id"
-passwd = "Password"
-
+passwd = "streetfighter@V4"
+# Log in to huggingface and grant authorization to huggingchat
 sign = Login(email, passwd)
 cookies = sign.login()
+
+# Save cookies to the local directory
 cookie_path_dir = "./cookies_snapshot"
 sign.saveCookiesToDir(cookie_path_dir)
 
+# Load cookies when you restart your program:
+# sign = login(email, None)
+# cookies = sign.loadCookiesFromDir(cookie_path_dir) # This will detect if the JSON file exists, return cookies if it does and raise an Exception if it's not.
+
+# Create a ChatBot
+chatbot = hugchat.ChatBot(cookies=cookies.get_dict())  # or cookie_path="usercookies/<email>.json"
+print(chatbot.chat("Hi!"))
 js = Javascript(
     """
     async function recordAudio() {
@@ -69,17 +81,31 @@ js = Javascript(
           };
           """
 )
+# record Audio HERE
+
+display(js)
+whisper = pipeline("automatic-speech-recognition", model = "openai/whisper-large-v2", chunk_length_s=30, device = "cuda:0")
+
+output = eval_js('recordAudio({})')
+with open('audio.wav','wb') as file:
+  binary = base64.b64decode(output)
+  file.write(binary)
+print('Recording save to:', file.name)
+speech_to_text = whisper("audio.wav")
+
+image_prompt = speech_to_text['text']
+print(image_prompt)
 display(js)
 
 output = eval_js('recordAudio({})')
 with open('audio.wav','wb') as file:
-    binary = base64.b64decode(output)
-    file.write(binary)
+  binary = base64.b64decode(output)
+  file.write(binary)
 print('Recording save to:', file.name)
-
 speech_to_text = whisper("audio.wav")
-image_prompt = speech_to_text['text']
 
+image_prompt = speech_to_text['text']
+print(image_prompt)
 System_job = '''
 [Your Expertise]: You are an expert in creating prompt using normal plain text sentences. If a user gives you certain sentences, you should engineer the sentence into a image creating perfect prompt.
 
@@ -93,7 +119,7 @@ System_job = '''
           4. What is your perferred background?: Good lighting, natural, realistic+++ and so on. For this you can give options by recognising the user's prompt and what background will suit for that.
  You should ask only 4 questions to the user which are given above and give options to them also. Once after you got all the queries cleared, then you can proceed to create the perfect prompt to generate an image and give it to them.
 
-[Additional Information] - There are certain things in image prompting which you should ask the user, but you can conclude the based on the user's sentence and the options they choose for your four questions, they are:
+[Additional Information] - There are certain things in image prompting which you can conclude the based on the user's sentence and the options they choose for your four questions, they are:
 1. The emphasis on the word "very" seems to improve generation quality! Repetition can also be used to emphasize subject terms. For example, if you want to generate an image of a planet with aliens, using the prompt A planet with aliens aliens aliens aliens aliens aliens aliens aliens aliens aliens aliens aliens will make it more likely that aliens are in the resultant image.
 2. If we want mountains without trees, we can use the prompt mountain | tree:-10. Since we weighted tree very negatively, they do not appear in the generated image. Weighted terms can be combined into more complicated prompts, like A planet in space:10 | bursting with color red, blue, and purple:4 | aliens:-10 | 4K, high quality
 3. Using a robust negative prompt, we can generate much more convincing iamges without deformed images. For example: studio medium portrait of Brad Pitt waving his hands, detailed, film, studio lighting, 90mm lens, by Martin Schoeller:6 | disfigured, deformed hands, blurry, grainy, broken, cross-eyed, undead, photoshopped, overexposed, underexposed, lowres, bad anatomy, bad hands, extra digits, fewer digits, bad digit, bad ears, bad eyes, bad face, cropped: -5
@@ -102,18 +128,9 @@ System_job = '''
 5. Emotion or Mood: Determine the emotional tone or mood you want the image to convey. This could be happiness, sadness, mystery, etc. Communicate this in your prompt.
 
 
-Atlast, keep in mind that the final prompt given by you should be short and sweet, less than 65 tokens, but still should convey all the necessary informations to generate the perfect image.
+Atlast, keep in mind that the final prompt given by you should be short and sweet, and compulsory less than 75 tokens, but still should convey all the necessary informations to generate the perfect image.
 '''
 print(chatbot.chat(System_job))
 print(chatbot.chat(image_prompt))
-print(chatbot.chat(input()))
-
-Engineered_prompt = chatbot.chat("Can you give me your engineered prompt alone inside quotations with all my selected options included")
-print("Engineered Prompt:")
-print(Engineered_prompt)
-
-prompt = Engineered_prompt
-image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
-image.save("image.png")
-final_image_path = "image.png"
-display(Image(filename=final_image_path))
+Engineered_prompt = chatbot.chat(input())  # Assuming this input is for the prompt
+print(Engineered_prompt)  # Print the full prompt
